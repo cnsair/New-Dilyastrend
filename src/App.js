6641ae1fb61701a834b13b1d26528a4842ab7b48
Ayinla Abeeb <abeebayinla@gmail.com>
2023-02-18 17:15:37 +0100

    project stup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Welcome to dilyastrend</h1>
+    </div>
+  );
+}
+
+export default App;
