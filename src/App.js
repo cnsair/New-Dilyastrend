197165f611e704f0a64a8e676c9e95683451b7d2
Ayinla Abeeb <abeebayinla@gmail.com>
2023-03-11 17:32:20 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Welcome to dilyastrend</h1>
-    </div>
+    <>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
