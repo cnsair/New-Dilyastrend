6d0acb59bf603c3eeae2701685b1cdcac08bf45a
Ayinla Abeeb <abeebayinla@gmail.com>
2023-03-11 17:31:42 +0100

    add tailwind color and daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      white: "#fff",
+      dark: "#000",
+      primarycolor: "#1fa5ec",
+      secondarycolor: "#7380f5",
+      grey: "#8B8B8B",
+      lightgrey: "#F2F2F2",
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
