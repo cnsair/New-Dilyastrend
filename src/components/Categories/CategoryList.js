a06b7b1234ba1abacdcf10fbf612568cfe76485a
Ayinla Abeeb <abeebayinla@gmail.com>
2023-05-08 00:29:53 +0100

    change app color

--- src/components/Categories/CategoryList.js
+++ src/components/Categories/CategoryList.js
@@ -12,51 +12,47 @@ const CategoryList = () => {
   return (
     <div className="grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 md:gap-7 lg:gap-7 xl:gap-7">
       <Link to="/">
-        <div className="bg-categories p-5 text-center flex justify-center relative rounded-lg hover:bg-deepcategories hover:transition hover:text-white">
-          <span className="absolute top-14 font-bold text-dark opacity-60 text-xl">
-            Art
-          </span>
-          <FaImage color="#93d7ff" size={100} />
+        <div className="bg-lightdark p-5 text-center flex justify-center relative rounded-lg hover:bg-white hover:transition hover:text-lightdark text-white">
+          <span className="absolute top-14 font-bold text-xl">Art</span>
+          <FaImage fill="#fff" size={100} />
         </div>
       </Link>
       <Link to="/">
-        <div className="bg-categories p-5 text-center flex justify-center relative rounded-lg hover:bg-deepcategories hover:transition hover:text-white">
-          <span className="absolute top-14 font-bold text-dark opacity-60 text-xl">
-            Music
-          </span>
-          <FaMusic color="#93d7ff" size={100} />
+        <div className="bg-lightdark p-5 text-center flex justify-center relative rounded-lg hover:bg-white hover:transition hover:text-lightdark text-white">
+          <span className="absolute top-14 font-bold text-xl">Music</span>
+          <FaMusic fill="#fff" size={100} />
         </div>
       </Link>
       <Link to="/">
-        <div className="bg-categories p-5 text-center flex justify-center relative rounded-lg hover:bg-deepcategories hover:transition hover:text-white">
-          <span className="absolute top-14 font-bold text-dark opacity-60 text-xl">
+        <div className="bg-lightdark p-5 text-center flex justify-center relative rounded-lg hover:bg-white hover:transition hover:text-lightdark text-white">
+          <span className="absolute top-14 font-bold text-xl">
             Domain Names
           </span>
-          <FaSearch color="#93d7ff" size={100} />
+          <FaSearch fill="#fff" size={100} />
         </div>
       </Link>
       <Link to="/">
-        <div className="bg-categories p-5 text-center flex justify-center relative rounded-lg hover:bg-deepcategories hover:transition hover:text-white">
-          <span className="absolute top-14 font-bold text-dark opacity-60 text-xl">
+        <div className="bg-lightdark p-5 text-center flex justify-center relative rounded-lg hover:bg-white hover:transition hover:text-lightdark text-white">
+          <span className="absolute top-14 font-bold text-xl">
             Virtual Worlds
           </span>
-          <FaDribbble color="#93d7ff" size={100} />
+          <FaDribbble fill="#fff" size={100} />
         </div>
       </Link>
       <Link to="/">
-        <div className="bg-categories p-5 text-center flex justify-center relative rounded-lg hover:bg-deepcategories hover:transition hover:text-white">
-          <span className="absolute top-14 font-bold text-dark opacity-60 text-xl">
+        <div className="bg-lightdark p-5 text-center flex justify-center relative rounded-lg hover:bg-white hover:transition hover:text-lightdark text-white">
+          <span className="absolute top-14 font-bold text-xl">
             Trading Cards
           </span>
-          <FaAddressCard color="#93d7ff" size={100} />
+          <FaAddressCard fill="#fff" size={100} />
         </div>
       </Link>
       <Link to="/">
-        <div className="bg-categories p-5 text-center flex justify-center relative rounded-lg hover:bg-deepcategories hover:transition hover:text-white">
-          <span className="absolute top-14 font-bold text-dark opacity-60 text-xl">
+        <div className="bg-lightdark p-5 text-center flex justify-center relative rounded-lg hover:bg-white hover:transition hover:text-lightdark text-white">
+          <span className="absolute top-14 font-bold text-xl">
             More Categories
           </span>
-          <FaMicrosoft color="#93d7ff" size={100} />
+          <FaMicrosoft fill="#fff" size={100} />
         </div>
       </Link>
     </div>
