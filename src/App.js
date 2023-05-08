989c90a82f316a80f5aad00e476364d8cf6737e4
Ayinla Abeeb <abeebayinla@gmail.com>
2023-05-08 03:40:37 +0100

    Add signup and signin route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Signup from "./pages/Auth/Signup";
+import Signin from "./pages/Auth/Signin";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/auth/signup" element={<Signup />} />
+          <Route path="/auth/signin" element={<Signin />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
