db09da1e9d32f0da16fe4912767d5c25cfc640bb
Ayinla Abeeb <abeebayinla@gmail.com>
2023-06-03 15:13:18 +0100

    complete auth validation and responsiveness

--- src/pages/Auth/Signin.js
+++ src/pages/Auth/Signin.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { FormProvider, useForm } from "react-hook-form";
+import { username_validation } from "../../utils/inputValidation";
 
 const Signin = () => {
   const methods = useForm();
@@ -23,9 +24,8 @@ const Signin = () => {
       <Container>
         <div className="p-6 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 md:gap-7 lg:gap-7 xl:gap-12 h-screen text-dark md:p-12 lg:p-12 xl:p-12">
           <div className="mt-12">
-            <p className="font-bold text-6xl mb-6 leading-tight">
-              Create, sell or <br />
-              collect digital items.
+            <p className="font-bold text-2xl md:text-4xl lg:text-6xl xl:text-6xl mb-3 md:mb-3 lg:mb-6 xl:mb-6 leading-tight">
+              Create, sell or collect digital items.
             </p>
             <p className="leading-normal font-normal">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -33,7 +33,7 @@ const Signin = () => {
               ut enim.
             </p>
           </div>
-          <div className="mt-12">
+          <div className="mt-1 md:mt-12 lg:mt-12 xl:mt-12">
             <div className="flex justify-center">
               <div className="p-6 shadow-lg rounded-lg border w-96">
                 <FormProvider {...methods}>
@@ -52,13 +52,7 @@ const Signin = () => {
                     {/* username */}
                     <div className="form-control w-full mb-3">
                       <div>
-                        <Input
-                          placeholder="username"
-                          id="username"
-                          name="username"
-                          type="text"
-                          autoComplete="off"
-                        />
+                        <Input {...username_validation} />
                       </div>
                     </div>
 
@@ -71,6 +65,12 @@ const Signin = () => {
                           name="password"
                           type={showPassword ? "text" : "password"}
                           autoComplete="off"
+                          validation={{
+                            required: {
+                              value: true,
+                              message: "required",
+                            },
+                          }}
                         />
                         <span
                           onClick={() => setShowPassword((prev) => !prev)}
